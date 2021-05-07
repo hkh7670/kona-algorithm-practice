f5df99cf8302622f103e0da2f47fe5d8650da517
GyuHo Han <hkh7670@gmail.com>
2021-05-07 12:45:27 +0900

    변수이름 변경

--- src/com/company/baekjoon/_1012/한규호_1012.java
+++ src/com/company/baekjoon/_1012/한규호_1012.java
@@ -8,7 +8,7 @@ public class 한규호_1012 {
 
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int order = 0;
+    static int idx = 0;
     static int m;
     static int n;
     static List<Integer> answers = new ArrayList<>();
@@ -27,7 +27,7 @@ public class 한규호_1012 {
             for (int j = 0; j < k; j++) {
                 int y = scanner.nextInt();
                 int x = scanner.nextInt();
-                table[x][y] = 1;
+                table[x][y] = 1; // 배추는 1로 표시
             }
             tables.add(table);
             visitedList.add(visited);
@@ -36,18 +36,18 @@ public class 한규호_1012 {
 
         for (int i = 0; i < testCase; i++) {
             int answer = 0;
-            m = tables.get(order)[0].length; // 테스트 케이스 농장의 가로길이
-            n = tables.get(order).length; // 테스트 케이스 농장의 세로길이
+            m = tables.get(idx)[0].length; // 테스트 케이스 농장의 가로길이
+            n = tables.get(idx).length; // 테스트 케이스 농장의 세로길이
             for (int x = 0; x < n; x++) {
                 for (int y = 0; y < m; y++) {
-                    if (tables.get(order)[x][y] == 1 && !visitedList.get(order)[x][y]) { // 배추가 있으면서 방문한 적 없는 위치인 경우?
+                    if (tables.get(idx)[x][y] == 1 && !visitedList.get(idx)[x][y]) { // 배추가 있으면서 방문한 적 없는 위치인 경우?
                         dfs(x, y); // 재귀함수로 인접 배추들을 전부 방문
                         answer++; // 인접 배추들을 전부 방문했으면 (= 재귀함수가 종료됐으면) 카운트 상승
                     }
                 }
             }
             answers.add(answer);
-            order++;
+            idx++;
         }
         for (int answer : answers) {
             System.out.println(answer);
@@ -56,13 +56,13 @@ public class 한규호_1012 {
     }
 
     static void dfs(int x, int y) {
-        visitedList.get(order)[x][y] = true; // 방문 흔적 남기기
+        visitedList.get(idx)[x][y] = true; // 방문 흔적 남기기
         for (int i = 0; i < 4; i++) { // 상, 하, 좌, 우의 인접한 부분에 방문한 적 없는 배추가 있는지 확인
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (ny >= 0 && ny < m && nx >= 0 && nx < n) { // 농장의 범위를 벗어나지 않으면서
-                if (tables.get(order)[nx][ny] == 1 && !visitedList.get(order)[nx][ny]) { // 배추가 있고, 방문한 적 없으면?
-                    dfs(nx, ny);
+                if (tables.get(idx)[nx][ny] == 1 && !visitedList.get(idx)[nx][ny]) { // 배추가 있고, 방문한 적 없으면?
+                    dfs(nx, ny); // 재귀함수로 인접 배추들을 전부 방문
                 }
             }
         }
