9f97c7e84b723a39d479ae24178a9ea249feaf38
GyuHo Han <hkh7670@gmail.com>
2021-05-16 18:51:42 +0900

    자잘한 수정

--- src/com/company/baekjoon/_1874/한규호_1874.java
+++ src/com/company/baekjoon/_1874/한규호_1874.java
@@ -11,17 +11,21 @@ public class 한규호_1874 {
         Stack<Integer> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
 
-        char plus = '+';
-        char minus = '-';
+        String lineBreak = "\n";
+        String plus = '+' + lineBreak;
+        String minus = '-' + lineBreak;
+
         int popIdx = 0; // pop할 때 사용할 배열 인덱스
         int pushNum = 1; // 오름차순으로 스택에 넣을 숫자 변수
         int n = Integer.parseInt(br.readLine());
+
         int[] numbers = new int[n];
 
         for (int i = 0; i < n; i++) {
             int number = Integer.parseInt(br.readLine());
             numbers[i] = number;
         }
+        br.close();
 
         while (popIdx < n) {
             if (pushNum == n + 1 && !stack.peek().equals(numbers[popIdx])) { // 더 이상 push할 숫자가 없으면서 (= pop밖에 할 수 없으면서) 스택의 Top에 있는 값이 요구되는 숫자와 다를경우 -> 수열 생성 불가능
@@ -34,12 +38,12 @@ public class 한규호_1874 {
                 if (stack.peek().equals(numbers[popIdx])) { // 스택의 Top에 있는 값이 요구되는 숫자와 같을 경우 -> pop 하면서 '-' 연산자 추가
                     stack.pop();
                     popIdx++;
-                    sb.append(minus).append("\n");
+                    sb.append(minus);
                     continue;
                 }
             }
             stack.push(pushNum++); // 위 두 조건에 해당 안되는 경우엔 stack에 오름차순으로 push
-            sb.append(plus).append("\n"); // push 후 '+' 연산자 추가
+            sb.append(plus); // push 후 '+' 연산자 추가
         }
 
         bw.write(sb.toString());
