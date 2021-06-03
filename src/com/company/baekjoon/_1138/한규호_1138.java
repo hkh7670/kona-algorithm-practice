76bbe50044cb465b140695a40647e9243ff8218f
GyuHo Han <hkh7670@gmail.com>
2021-06-03 21:13:29 +0900

    백준 1138번 문제 풀이

--- src/com/company/baekjoon/_1138/한규호_1138.java
+++ src/com/company/baekjoon/_1138/한규호_1138.java
@@ -1,7 +1,37 @@
 package com.company.baekjoon._1138;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class 한규호_1138 {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] leftCntArr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] line = new int[n];
+
+        for (int leftIdx = 0; leftIdx < n; leftIdx++) {
+            for (int lineIdx = 0; lineIdx < n; lineIdx++) {
+                if (leftCntArr[leftIdx] == 0 && line[lineIdx] == 0) {
+                    line[lineIdx] = leftIdx + 1;
+                    break;
+                }
+                if (line[lineIdx] == 0) { // leftCntArr[leftIdx] 보다 작은 값이 자리를 잡지 않은 경우에만 차감
+                    leftCntArr[leftIdx]--;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            bw.write(line[i] + " ");
+            bw.flush();
+        }
+        bw.close();
+        br.close();
     }
 }
