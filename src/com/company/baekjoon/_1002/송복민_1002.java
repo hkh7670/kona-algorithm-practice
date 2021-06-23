47e0e24f9bfabeb0e50f435cb1a4d807875ef6c3
송복민 <bockmin@gmail.com>
2021-06-24 00:42:39 +0900

    주석 추가

--- src/com/company/baekjoon/_1002/송복민_1002.java
+++ src/com/company/baekjoon/_1002/송복민_1002.java
@@ -21,14 +21,14 @@ public class 송복민_1002 {
 			int y2=Integer.parseInt(st.nextToken());
 			int r2=Integer.parseInt(st.nextToken());
 			
-			int answer=2;
+			int answer=2;//제곱이라서 최대 해는 2개밖에 나오지 않음
 			double distance=Math.sqrt(Math.pow((x1-x2), 2)+Math.pow((y1-y2), 2));
-			if(x1==x2 && y1==y2 && r1!=r2) {
-				answer=-1;
-			}else if((x1==x2 && y1==y2 && r1==r2) ||distance>r1+r2 || distance<Math.abs(r1-r2)) {
-				answer=0;
+			if(x1==x2 && y1==y2 && r1==r2) {
+				answer=-1;//부정 (해가 무한대)
+			}else if((x1==x2 && y1==y2 && r1!=r2) ||distance>r1+r2 || distance<Math.abs(r1-r2)) {
+				answer=0;//불능 (해가 없음)
 			}else if(distance==r1+r2 || distance==Math.abs(r1-r2)) {
-				answer=1;
+				answer=1;//해가 딱 하나일 떄
 			}
 			System.out.println(answer);
 		}
