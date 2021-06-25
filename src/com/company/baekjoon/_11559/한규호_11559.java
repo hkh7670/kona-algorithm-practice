9f145aaf69f9b232a1b4e10379cf668d2a22c610
GyuHo Han <hkh7670@gmail.com>
2021-06-26 03:19:51 +0900

    -

--- src/com/company/baekjoon/_11559/한규호_11559.java
+++ src/com/company/baekjoon/_11559/한규호_11559.java
@@ -53,6 +53,8 @@ public class 한규호_11559 {
                         dfs(i, j, item);
                         if (puyoCnt >= 4) { // 인접 뿌요가 4개 이상 잡히면 뿌요 터짐 발생 -> 점으로 치환
                             explode();
+                            bw.write("\n---visit(explode) map---\n");
+                            print2ndArr(visit);
                             bw.write("\n---explode---\n");
                             print2ndArr(field);
                             isExplode = true;
